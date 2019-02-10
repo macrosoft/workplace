1d466597e1992dcae3e28367805e8f5238ef92e4
Artyom Zagidulin <p0chta@yandex.ru>
2019-02-10 19:08:50 +0300

    adding config file. connect to wi-fi

--- workplace.ino
+++ workplace.ino
@@ -1,4 +1,6 @@
 #include <ChainableLED.h>
+#include <ESP8266WiFi.h>
+#include "config.h"
 
 #define CLK_LED_PIN D0
 #define DATA_LED_PIN D1
@@ -8,6 +10,17 @@ ChainableLED led(CLK_LED_PIN, DATA_LED_PIN, 1);
 void setup() {
   led.init();
   led.setColorRGB(0, random(255), random(255), random(255));
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
