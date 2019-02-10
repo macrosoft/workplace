610ab3ccb4ac570b6b0dc8b3ce438f36e42c5b16
Artyom Zagidulin <p0chta@yandex.ru>
2019-02-10 16:18:49 +0300

    adding ChainableLED.h

--- workplace.ino
+++ workplace.ino
@@ -1,4 +1,13 @@
+#include <ChainableLED.h>
+
+#define CLK_LED_PIN D0
+#define DATA_LED_PIN D1
+
+ChainableLED led(CLK_LED_PIN, DATA_LED_PIN, 1);
+
 void setup() {
+  led.init();
+  led.setColorRGB(0, random(255), random(255), random(255));
 }
 
 void loop() {
