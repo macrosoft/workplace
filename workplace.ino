b1520ca92ead5a634b05218dc36ca5b5ff980a0b
Artyom Zagidulin <p0chta@yandex.ru>
2019-02-11 20:35:25 +0300

    http updater instead OTA

--- workplace.ino
+++ workplace.ino
@@ -1,11 +1,14 @@
-#include <ArduinoOTA.h>
 #include <ChainableLED.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include "config.h"
 
 #define CLK_LED_PIN D0
 #define DATA_LED_PIN D1
 
+ESP8266WebServer webServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
 ChainableLED led(CLK_LED_PIN, DATA_LED_PIN, 1);
 
 void setup() {
@@ -14,18 +17,29 @@ void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+  Serial.print("\r\nConnect to ");
+  Serial.print(WIFI_SSID);
+  for (byte i = 0; i < 80; i++) {
+    if (WiFi.status() == WL_CONNECTED)
+      break;
+    delay(250);
+    Serial.print(".");
   }
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  ArduinoOTA.setHostname("workplace");
-  ArduinoOTA.begin();
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("[OK]");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("[FAILED]");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("workplace", PASSWORD);
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
+  }
+  httpUpdater.setup(&webServer, "/update", USERNAME, PASSWORD);
+  webServer.begin();
 }
 
 void loop() {
-  ArduinoOTA.handle();
+  webServer.handleClient();
 }
